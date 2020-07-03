c77ae71e5fa8534773c0c5bfafb4f34952b1fcb9
bananabrick <a26nair@edu.uwaterloo.ca>
2020-07-02 21:34:34 -0400

    remove incorrect param

--- src/anal.py
+++ src/anal.py
@@ -10,7 +10,6 @@ def mess():
             data.DataSets.GENUINE: 1000,
             data.DataSets.T_1: 1000
         },
-        num_trees=100,
         cols_to_build=data.TO_BUILD,
         cols_to_keep=data.TO_KEEP,
         classify_on="is_bot"
