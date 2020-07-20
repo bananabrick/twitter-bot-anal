7de8d777aa3ecec3aac39637d9c97513a4782621
Arjun Dhaliwal <arjun.p.dhaliwal@gmail.com>
2020-07-19 22:51:24 -0400

    add adaboost

---
+++ src/ada_analysis.py
@@ -0,0 +1,54 @@
+import data
+import util
+import pandas
+import useful_configs
+
+from sklearn.ensemble import AdaBoostClassifier
+
+def base_test():
+    base_config = useful_configs.ALL
+
+    sample, is_bot = base_config.sample(
+        {
+            data.DataSets.GENUINE: 500,
+            data.DataSets.T_1: 200,
+            data.DataSets.T_2: 100,
+            data.DataSets.T_3: 200,
+        },
+        random_state=12345
+    )
+
+    ada = AdaBoostClassifier()
+    
+    ada.fit(sample, is_bot)
+
+    test_sample, is_bot_test = base_config.sample(
+        {
+            data.DataSets.GENUINE: 1300,
+            data.DataSets.T_1: 600,
+            data.DataSets.T_2: 100,
+            data.DataSets.T_3: 600,
+
+        },
+        random_state=23456
+    )
+
+    ada_prediction = ada.predict(test_sample)
+    compare = pandas.DataFrame(
+        {
+            'is_bot_original': is_bot_test,
+            'ada_is_bot_predict': ada_prediction
+        }
+    )
+
+    ada_importances = list(zip(sample.columns.values, ada.feature_importances_))
+    util.plot_feature_importances(
+        "ada", "AdaBoost feature importances",
+        "Gini Importance", ada_importances
+    )
+
+    print(util.accuracy(compare, "is_bot_original", "ada_is_bot_predict"))
+
+
+if __name__ == "__main__":
+    base_test()
