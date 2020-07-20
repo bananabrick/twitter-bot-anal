c7c3a62b61bfc53f12315ddf31acd11a989de5ef
Arjun Dhaliwal <arjun.p.dhaliwal@gmail.com>
2020-07-20 10:54:53 -0400

    make ada more lean

--- src/useful_configs.py
+++ src/useful_configs.py
@@ -39,6 +39,43 @@ ALL = config.ConfigVars(
     classify_on="is_bot"
 )
 
+ADA = config.ConfigVars(
+    name="all_features",
+    cols_to_build=[
+        "is_bot",
+        "has_default_profile_image", 
+        "no_screen_name",
+        "language_not_empty",
+        "description_contains_url",
+        "description_length",
+        "geo_enabled",
+        "has_name",
+        "fr_fo_ratio_gt_100",
+        "fr_fo_ratio_gt_50",
+        "fr_fo_ratio",
+        "name_edit_distance",
+    ],
+    cols_to_keep=[
+        "is_bot",
+        #"has_default_profile_image", 
+        #"no_screen_name",
+        "language_not_empty",
+        #"description_contains_url",
+        "description_length",
+        #"fr_fo_ratio_gt_100",
+        #"fr_fo_ratio_gt_50",
+        #"has_name",
+        "geo_enabled",
+        "followers_count",
+        "friends_count",
+        "statuses_count",
+        "fr_fo_ratio",
+        "name_edit_distance",
+        "listed_count"
+    ],
+    classify_on="is_bot"
+)
+
 NB = config.ConfigVars(
     name="all_features",
     cols_to_build=[
